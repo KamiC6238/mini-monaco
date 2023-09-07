632d2d0cc3293cd3dbec999c93ade83b0da7a0f8
kobayashi <823913645@qq.com>
2023-09-07 21:33:17 +0800

    chore: use worker url instead of worker instance

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,36 @@
-export { default as createEditor } from './core'
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js'
+
+import 'monaco-editor/esm/vs/language/typescript/monaco.contribution'
+import 'monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution'
+import 'monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution'
+import 'monaco-editor/esm/vs/editor/contrib/suggest/browser/suggestController.js'
+
+import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
+type MonacoEditorOptions = monaco.editor.IStandaloneEditorConstructionOptions
+
+window.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label === 'javascript' || label === 'typescript')
+      return new TsWorker()
+
+    return new EditorWorker()
+  },
+}
+
+const defaultEditorOptions: MonacoEditorOptions = {
+  language: 'javascript',
+  theme: 'vs-dark',
+  automaticLayout: true,
+  minimap: {
+    enabled: false,
+  },
+}
+
+export function createEditor(element: HTMLElement, options?: MonacoEditorOptions) {
+  return monaco.editor.create(element, {
+    ...defaultEditorOptions,
+    ...(options ?? {}),
+  })
+}
